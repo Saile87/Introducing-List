f4d1c524a6b28dadc14c194c2dd26279323af346
Elias Breitenbach <elias.b@me.com>
2023-04-28 22:55:02 +0200

    Create a project in Github

---
+++ Introducing List/Introducing List/Introducing_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introducing_ListApp.swift
+//  Introducing List
+//
+//  Created by Elias Breitenbach on 28.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Introducing_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
