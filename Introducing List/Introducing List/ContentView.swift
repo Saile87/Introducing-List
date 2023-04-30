a1471a6feb47ab53337267132e369645e458bb83
Elias Breitenbach <elias.b@me.com>
2023-04-30 21:18:46 +0200

    Loading resources from your app bundle

--- Introducing List/Introducing List/ContentView.swift
+++ Introducing List/Introducing List/ContentView.swift
@@ -9,26 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        let people = ["Finn", "Leia", "Luke", "Rey"]
-        List(people, id: \.self) {
-            Text($0)
-            //            List(0..<5) {
-            //                Text("Dynamic row \($0)")
-            //                Section("Section 1") {
-            //                    Text("Static row 1")
-            //                    Text("Static row 2")
-            //                }
-            //                Section("Section 2") {
-            //                    ForEach(0..<5) {
-            //                        Text("Dynamic row \($0)")
-            //                    }
-            //                }
-            //                Section("Section 3") {
-            //
-            //                    Text("Static row 3")
-            //                }
-        }
-        .listStyle(.grouped)
+
+            if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+                // we found the file in our bundle!
+                
+                if let fileContents = try? String(contentsOf: fileURL) {
+                    // we loaded the file into a string!
+                }
+            }
+            let people = ["Finn", "Leia", "Luke", "Rey"]
+            List(people, id: \.self) {
+                Text($0)
+                //            List(0..<5) {
+                //                Text("Dynamic row \($0)")
+                //                Section("Section 1") {
+                //                    Text("Static row 1")
+                //                    Text("Static row 2")
+                //                }
+                //                Section("Section 2") {
+                //                    ForEach(0..<5) {
+                //                        Text("Dynamic row \($0)")
+                //                    }
+                //                }
+                //                Section("Section 3") {
+                //
+                //                    Text("Static row 3")
+                //                }
+            }
+            .listStyle(.grouped)
     }
 }
 
